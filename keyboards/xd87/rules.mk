238527ec397e98f6fc7669ed2f8dd3a47801c529
Dirk Van Haerenborgh <dirk.vanhaerenborgh@aloxy.io>
2020-12-08 20:37:06 +0100

    my keyboard config

--- keyboards/xd87/rules.mk
+++ keyboards/xd87/rules.mk
@@ -22,9 +22,9 @@ COMMAND_ENABLE = yes        # Commands for debug and configuration
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 # if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-NKRO_ENABLE = no            # USB Nkey Rollover
+NKRO_ENABLE = yes           # USB Nkey Rollover
 BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality on B7 by default
-RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+RGBLIGHT_ENABLE = yes        # Enable keyboard RGB underglow
 MIDI_ENABLE = no            # MIDI support
 UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
@@ -33,3 +33,5 @@ FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
 
 # Supported layouts
 LAYOUTS = tkl_ansi tkl_iso
+
+DYNAMIC_MACRO_ENABLE = yes
